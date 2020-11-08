753f0a0c33b45e1c66be7012d509cd3f07464a83
kimiko-i <73594659+kimiko-i@users.noreply.github.com>
2020-11-08 22:13:31 +0900

    Revert "Merge branch 'second_training' of https://github.com/kimiko-i/training_curriculum into second_training"
    This reverts commit 76838c01aa19ad6ae405b4d854e049f42a3040d7, reversing
    changes made to d5df2cf5c0bb763ae43a1699b474d763eeb77f00.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,6 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
       wday_num = Date.today.wday #Date.today.wdayを利用して添字となる数値を得る
       #もしもwday_numが7以上であれば、7を引く
       if wday_num >= 7
